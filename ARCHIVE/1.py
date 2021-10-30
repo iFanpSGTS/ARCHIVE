391246e6eb2dacdc0bc4670df2922ca0066d6374
iFanpS <89406191+iFanID@users.noreply.github.com>
2021-10-30 07:16:54 +0700

    Update 1.py

--- ARCHIVE/1.py
+++ ARCHIVE/1.py
@@ -6,12 +6,10 @@ data = []
 page = 'https://quran.com'
 web = requests.get(page)
 semua_element = bp(web.text, 'html.parser')
-
+x = 0
 for quran in semua_element.findAll('div', class_='surah-card'):
+    x = x + 1
     nama_surah = quran.h5.text
     arti_surah = quran.find('p', class_='english').text
-    data.append({'Surah':nama_surah,'Arti':arti_surah})
-dumpin = json.dumps(data, indent=2, ensure_ascii=False)
-with open('hasil_surah.json','w') as surah_write:
-    surah_write.write(dumpin)
-    surah_write.close()
+    data.append({'No':x,'Surah':nama_surah,'Arti':arti_surah})
+print(json.dumps(data, indent=2, ensure_ascii=False))
